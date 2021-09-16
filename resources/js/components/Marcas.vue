e4467dd4f336a75ae5bbab2cebda42384b8b1759
Vinicius Moreira <vmarv@hotmail.com>
2021-09-16 15:59:37 -0300

    envio padrão

---
+++ resources/js/components/Marcas.vue
@@ -0,0 +1,162 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+
+                <!-- Inicio do card de busca -->
+                <card-component titulo="Busca de marcas">
+                    <template v-slot:conteudo>
+                        <div class="form-row">
+                            <div class="form-group col">
+
+                                <input-container-component titulo="ID" id="inputID" id-help="idHelp" texto-ajuda="Opcional. Informe o ID do registro">
+                                    <input type="number" class="form-control" id="inputID" aria-describedby="idHelp" placeholder="ID">
+                                </input-container-component>
+
+                            </div>
+                            <div class="form-group col">
+
+                                <input-container-component titulo="Nome da marca" id="inputNome" id-help="nomeHelp" texto-ajuda="Opcional. Informe o nome da marca">
+                                    <input type="text" class="form-control" id="inputNome" aria-describedby="nomeHelp" placeholder="Nome da marca">
+                                </input-container-component>
+
+                            </div>
+                        </div>
+                    </template>
+
+                    <template v-slot:rodape>
+                        <button type="submit" class="btn btn-primary btn-sm float-right">Pesquisar</button>
+                    </template>
+                </card-component>
+                <!-- fim do card de busca -->
+
+                <!-- Inicio do card de listagem de marcas -->
+                <card-component titulo="Relação de marcas">
+                    <template v-slot:conteudo>
+                        <table-component :dados="marcas" :titulos="['id', 'nome', 'imagem']"></table-component>
+                    </template>
+
+                    <template v-slot:rodape>
+                        <button type="button" class="btn btn-primary btn-sm float-right" data-toggle="modal" data-target="#modalMarca">Adicionar</button>
+                    </template>
+                </card-component>
+                <!-- Fim do card de listagem de marcas -->
+
+                <modal-component id="modalMarca" titulo="Modal marcas">
+
+                    <template v-slot:alertas>
+                        <alert-component tipo="success" :detalhes="transacaoDetalhes" titulo="Cadastro realizado com sucesso" v-if="transacaoStatus == 'adicionado'"></alert-component>
+                        <alert-component tipo="danger" :detalhes="transacaoDetalhes" titulo="Erro ao tentar cadastrar a marca" v-if="transacaoStatus == 'erro'"></alert-component>
+                    </template>
+
+                    <template v-slot:conteudo>
+                        <div class="form-group">
+                            <input-container-component titulo="Nome da marca" id="novoNome" id-help="novoNomeHelp" texto-ajuda="Informe o nome da marca">
+                                <input type="text" class="form-control" id="novoNome" aria-describedby="novoNomeHelp" placeholder="Nome da marca" v-model="nomeMarca">
+                            </input-container-component>
+                            {{nomeMarca}}
+                        </div>
+
+                        <div class="form-group">
+                            <input-container-component titulo="Imagem" id="novoImagem" id-help="novoImagemHelp" texto-ajuda="Selecione uma imagem no formato PNG">
+                                <input type="file" class="form-control-file" id="novoImagem" aria-describedby="novoImagemHelp" placeholder="Selecione uma imagem" @change="carregarImagem($event)">
+                            </input-container-component>
+                            {{arquivoImagem}}
+                        </div>
+                    </template>
+
+                    <template v-slot:rodape>
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+                        <button type="button" class="btn btn-primary" @click="salvar()">Salvar</button>
+                    </template>
+
+                </modal-component>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        computed: {
+            token() {
+                let token = document.cookie.split(';').find(indice => {
+                    return indice.includes('token=')
+                })
+                token = token.split('=')[1]
+                token = 'Bearer ' + token
+
+                return token
+            }
+        },
+        data() {
+            return {
+                urlBase:'http://localhost/api/v1/marca',
+                nomeMarca: '',
+                arquivoImagem: [],
+                transacaoStatus: '',
+                transacaoDetalhes: {},
+                marcas: []
+            }
+        },
+        methods: {
+            carregarLista() {
+
+                let config = {
+                    headers: {
+                        'Accept': 'application/json',
+                        'Authorization': this.token
+                    }
+                }
+
+                axios.get(this.urlBase, config)
+                    .then(response => {
+                        this.marcas = response.data
+                        console.log(this.marcas)
+                    })
+                    .catch(errors => {
+                        console.log(errors)
+                    })
+            },
+            carregarImagem(e) {
+                this.arquivoImagem = e.target.files
+            },
+            salvar() {
+                console.log(this.nomeMarca, this.arquivoImagem[0])
+
+                let formData = new FormData()
+                formData.append('nome', this.nomeMarca)
+                formData.append('imagem', this.arquivoImagem[0])
+
+                let config = {
+                    headers: {
+                        'Content-Type': 'multipart/form-data',
+                        'Accept': 'application/json',
+                        'Authorization': this.token
+                    }
+                }
+
+                axios.post(this.urlBase, formData, config)
+                    .then(response => {
+                        this.transacaoStatus = 'adicionado'
+                        this.transacaoDetalhes = {
+                            mensagem: 'ID do registro: ' + response.data.id
+                        }
+                        //console.log(response)
+                    })
+                    .catch(errors => {
+                        this.transacaoStatus = 'erro'
+                        this.transacaoDetalhes = {
+                            mensagem: errors.response.data.message,
+                            dados: errors.response.data.errors
+                        }
+                        //console.log(errors.response.data.message)
+                    })
+            }
+        },
+        mounted() {
+            this.carregarLista()
+        }
+    }
+</script>
---
+++ resources/js/components/Table.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th scope="col" v-for="t, key in titulos" :key="key"  class="text-uppercase">{{t}}</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="obj in dados" :key="obj.id">
+                    <td v-if="titulos.includes(chave)" v-for="valor, chave in obj" :key="chave">
+                        <span v-if="chave == 'imagem'">
+                            <img :src="'/storage/' + valor" width="30" height="30">
+                        </span>
+                        <span v-else>
+                            {{ valor }}
+                        </span>
+                    </td>
+                    <!--<th scope="row">{{m.id}}</th>
+                    <td>{{m.nome}}</td>
+                    <td><img :src="'/storage/' + m.imagem" width="30" height="30"></td>-->
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['dados', 'titulos']
+    }
+</script>
